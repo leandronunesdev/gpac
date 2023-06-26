b2bd4ef746e8f3bafd14e413da7f40e6b8889918
Leandro Nunes <nunes.pessoal@gmail.com>
2023-06-26 16:35:30 -0300

    video text

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -14,17 +14,30 @@ const Video = () => {
   }, [isClicked, video]);
 
   return (
-    <div className="grayscale">
+    <div>
       <div>
         {!isClicked && (
-          <img
-            src={PlayIcon}
-            className="absolute left-0 right-0 mx-auto mt-96 cursor-pointer animate-fade-in-right"
-          />
+          <div className="relative z-50">
+            <img
+              src={PlayIcon}
+              className="absolute left-0 right-0 mx-auto mt-96 cursor-pointer animate-fade-in-right "
+              onClick={() => setIsClicked(!isClicked)}
+            />
+            <div className="absolute left-[115px] right-0 top-[313px] animate-fade-in-right w-[619px]">
+              <h1 className="text-red font-bebasNeue uppercase text-[108px] opacity-70">
+                Our newest video
+              </h1>
+              <p className="text-[17px] font-raleway leading-[34px] max-w-[612px] text-white ">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a
+                tincidunt dui, vitae posuere ipsum. Praesent vehicula nunc eget
+                risus vehicula rutrum. Vestibulum pulvinar sapien tellus, sit
+                amet ultrices turpis vestibulum at.
+              </p>
+            </div>
+          </div>
         )}
       </div>
       <video
-        width="100%"
         autoPlay
         loop
         muted={!isClicked}
@@ -33,6 +46,7 @@ const Video = () => {
         controlsList="nodownload nofullscreen noplaybackrate"
         onClick={() => setIsClicked(!isClicked)}
         onPause={() => setIsClicked(false)}
+        className="grayscale"
       >
         <source src={VideoFile} />
         Your browser does not support the video tag.
